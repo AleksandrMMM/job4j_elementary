938cf4c1f36b5e8bc7483a574466c1dd86a5dc3b
Aleks <no>
2021-04-17 22:11:20 +0300

    tests

---
+++ src/ru/job4j/oop/PointTest.java
@@ -0,0 +1,49 @@
+package oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.condition.Point;
+
+public class PointTest {
+    @Test
+    public void when00to20then2() {
+        int expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void when20to20then2() {
+        int expected = 2;
+        int x1 = 2;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void when00to02then2() {
+        int expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+        public void when20to00then2() {
+            int expected = 2;
+            int x1 = 2;
+            int y1 = 0;
+            int x2 = 0;
+            int y2 = 0;
+            double out = Point.distance(x1, y1, x2, y2);
+            Assert.assertEquals(expected, out, 0.01);
+        }
+
+}
