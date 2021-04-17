941d8b2e56fcfcb26280c33fd15c4e147780ef1c
Aleks <no>
2021-04-17 22:42:27 +0300

    1.7. Состояние объекта new

--- src/ru/job4j/oop/Battery.java
+++ src/ru/job4j/oop/Battery.java
@@ -1,12 +1,22 @@
-package ru.job4j.oop;
+package oop;
 
 public class Battery {
 
     private int load;
 
+    public Battery(int load) {
+        this.load = load;
+    }
+
     public void exchange(Battery another) {
-        this.load --;
-        another.load ++;
+        if (this.load > another.load) {
+            this.load += another.load;
+            another.load = 0;
+        }else {
+            this.load += another.load;
+            another.load = another.load - this.load;
+        }
+
     }
 
     public int getLoad() {
@@ -18,13 +28,11 @@ public class Battery {
     }
 
     public static void main(String[] args) {
-        Battery battery1 = new Battery();
-        Battery battery2 = new Battery();
-        Battery battery3 = new Battery();
+        Battery battery1 = new Battery(23);
+        Battery battery2 = new Battery(4);
+        Battery battery3 = new Battery(13);
 
-        battery1.setLoad(11);
-        battery2.setLoad(12);
-        battery3.setLoad(13);
+
 
         battery1.exchange(battery2);
         System.out.printf(" заряд 1 %d, заряд 2 %d", battery1.getLoad(), battery2.getLoad());
